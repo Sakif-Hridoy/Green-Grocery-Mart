0ab7a98045db783626fcb89463197b27e8d385fc
Sakif-Hridoy <sakifworkspace@gmail.com>
2026-07-25 05:04:32 +0600

    myArray index comment

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const myArray = ["Sakif","Sakib","Hridoy","Ashfaq"]
 for (let i=0; i <myArray.length; i++){
     console.log(i) // ouput hisebe array er index gula dekhabe serially ba loop kore mane indexgula dekhabe ar ki
     console.log(myArray)// pura myArray ta array akare joto length array er totobar print kore dekhabe....array akarei mane length 5 hole 5 ta array hobe
-    // console.log(myArray[i]);
+    console.log(myArray[i]);// ar eta to specific vabe point korar jonno ... ar ha eibarei kintu asol ouput dekhabe orthat mayarray te jehetu i ke index hisebe dhoraya deoa hoise tai array er eobgula element print hobe
 
 }
