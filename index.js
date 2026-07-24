3408028770045bc63e4db7d17834f544498ee082
Sakif-Hridoy <sakifworkspace@gmail.com>
2026-07-25 05:02:37 +0600

    myArray comment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const myArray = ["Sakif","Sakib","Hridoy","Ashfaq"]
 
 for (let i=0; i <myArray.length; i++){
-    console.log(i)
-    console.log(myArray)
-    console.log(myArray[i]);
+    console.log(i) // ouput hisebe array er index gula dekhabe serially ba loop kore mane indexgula dekhabe ar ki
+    console.log(myArray)// pura myArray ta array akare joto length array er totobar print kore dekhabe....array akarei mane length 5 hole 5 ta array hobe
+    // console.log(myArray[i]);
 
 }
